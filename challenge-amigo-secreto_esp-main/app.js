f51aec46779200838fda31aa5cb2742a190be750
Loc78 <leonardo78obregon@gmail.com>
2025-02-03 20:37:34 -0600

    Add files via upload

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,12 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = ["Carlos", "Cesar", "Alejandro", "Luis", "Juan", "Pedro", "Manuel", "Javier", "Ricardo", "Roberto"];
+amigos.push("Carlos");
+amigos.push("Cesar");
+amigos.push("Alejandro");
+amigos.push("Luis");
+amigos.push("Juan");
+amigos.push("Pedro");
+amigos.push("Manuel");
+amigos.push("Javier");
+amigos.push("Ricardo");
+amigos.push("Roberto");
